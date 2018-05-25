c17283e6d75e23b207d00ed488fbdc006586f219
kina <kina.girl@gmail.com>
2018-05-25 20:22:14 +0300

    Refactor code for simple app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react'
 import SelectedPosts from './Components/SelectedPosts/SelectedPostsContainer'
 import Posts from './Components/Posts/PostsListContainer'
 class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      selected: false
-    }
-  }
   componentWillMount () {
     let posts = require('./posts.json').array
     this.props.savePosts(posts)
