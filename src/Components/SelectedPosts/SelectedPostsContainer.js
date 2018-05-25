4378f8fbb9d08fea6e5b49a1c003d7139ebcdbe4
kina <kina.girl@gmail.com>
2018-05-25 19:47:34 +0300

    Multiple memorization with closure

--- src/Components/SelectedPosts/SelectedPostsContainer.js
+++ src/Components/SelectedPosts/SelectedPostsContainer.js
@@ -2,27 +2,11 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import savedPosts from './SelectedPosts'
 import { removeSelectedPosts } from '../../Actions'
-import { selectedPosts } from '../../Selectors/index'
+// import { selectedPosts } from '../../Selectors/index'
 import { makeSelectedPosts } from '../../Selectors/index'
 
 // const mapStateToProps = (state, ownProps) => {
-  // This is how selector should NOT looks like
-
-  // let selectedPosts = []
-  // state.selectedPosts.forEach(id => {
-  //   let post = state.posts.all.find(post => post.id === id)
-  //   if (post) {
-  //     selectedPosts.push(post)
-  //   }
-  // })
   // return {
-  //   selectedPosts: selectedPosts
-  // }
-
-  // This is how selector should looks like
-
-  // return {
-    // selectedPosts: selectedPosts(state)
     // selectedPosts: selectedPosts(state, ownProps)
   // }
 // }
--- src/Selectors/index.js
+++ src/Selectors/index.js
@@ -1,59 +1,28 @@
 import { createSelector } from 'reselect'
 
-// Just selection logic without  memorization
-
-// export const selectedPosts = state => {
-//   return state.posts.all.filter(post => {
-//     return state.selectedPosts.findIndex(id => id === post.id) >= 0
-//   })
-// }
 
 // reselect with memorization
-
 // First create selector that will be used to calculate data
-
 export const allPosts = state => state.posts.all
 export const selectedPostsIds = (state, props) => state.selectedPosts
 const category = (state, props) => props.category
 
-// Then use reselect's createSelector function to memorize the selector
-
-// export const selectedPosts = createSelector(
-//   allPosts, selectedPostsIds,
-//   (allPosts, selectedPostsIds) => {
-//     return allPosts.filter(post => {
-//       return selectedPostsIds.findIndex(id => id === post.id) >= 0
-//     })
-//   })
-
 // createSelector function for multiple components
-
 // export const selectedPosts = createSelector(
 //     allPosts, selectedPostsIds, category,
 //     (allPosts, selectedPostsIds, category) => {
-//       let selectedPosts = []
-//       selectedPostsIds.forEach(id => {
-//         let post = allPosts.filter(post => post.category === category).find(post => post.id === id)
-//         if (post) {
-//           selectedPosts.push(post)
-//         }
+//       return allPosts.filter(post => {
+//         return selectedPostsIds.findIndex(id => id === post.id && category === post.category) >= 0
 //       })
-//       return selectedPosts
 //     })
 
-// createSelector correct function for multiple components
-
+// createSelector valid function for multiple components
 export const makeSelectedPosts = () => {
   return createSelector(
     allPosts, selectedPostsIds, category,
     (allPosts, selectedPostsIds, category) => {
-      let selectedPosts = []
-      selectedPostsIds.forEach(id => {
-        let post = allPosts.filter(post => post.category === category).find(post => post.id === id)
-        if (post) {
-          selectedPosts.push(post)
-        }
+      return allPosts.filter(post => {
+        return selectedPostsIds.findIndex(id => id === post.id && category === post.category) >= 0
       })
-      return selectedPosts
     })
 }
